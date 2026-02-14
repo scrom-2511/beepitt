1350ed367095eddd4ce8793c9b710740a081f2d0
scrom <formain337@gmail.com>
2026-02-14 08:26:13 +0000

    add: LoadMoreDiv component

---
+++ frontend/src/components/dashboard/LoadMoreDiv.tsx
@@ -0,0 +1,30 @@
+import type React from "react";
+import { Loading } from "../Loading";
+
+type LoadMoreDivProps = {
+  hasNextPage: boolean;
+  loadMoreRef: React.RefObject<HTMLDivElement | null>;
+  isFetchingNextPage: boolean;
+};
+
+const LoadMoreDiv = ({
+  hasNextPage,
+  loadMoreRef,
+  isFetchingNextPage,
+}: LoadMoreDivProps) => {
+  return (
+    <div>
+      {hasNextPage && (
+        <div ref={loadMoreRef} className="col-span-full text-center py-5">
+          {isFetchingNextPage ? (
+            <Loading title="Loading" />
+          ) : (
+            "Scroll to load more"
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LoadMoreDiv;
