d51b1606f7bd4a43ba6fa68a73c3787b53453801
scrom <formain337@gmail.com>
2026-02-14 08:01:50 +0000

    add: cardanimation component

---
+++ frontend/src/components/dashboard/CardAnimation.tsx
@@ -0,0 +1,23 @@
+import { motion } from "framer-motion";
+import type React from "react";
+
+const CardAnimation = ({
+  children,
+  i,
+}: {
+  children: React.ReactNode;
+  i: number;
+}) => {
+  return (
+    <motion.div
+      layout
+      initial={{ y: 50, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      exit={{ y: -50, opacity: 0 }}
+      transition={{ delay: 0.05 * i, ease: "easeIn" }}
+      className="cursor-pointer"
+    ></motion.div>
+  );
+};
+
+export default CardAnimation;
