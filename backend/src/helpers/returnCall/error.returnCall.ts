678ba79662987bcaf8d86a3f404b70c5e7b43eb0
scrom <formain337@gmail.com>
2026-02-22 11:36:08 +0000

    add: returncall helper

---
+++ backend/src/helpers/returnCall/error.returnCall.ts
@@ -0,0 +1,19 @@
+import { Response } from 'express';
+import { ERROR_METADATA, ErrorCode, HttpStatus } from '../../types/errorCodes';
+
+export const errorReturnCall = (
+  res: Response,
+  statusCode: HttpStatus,
+  errorCode: ErrorCode,
+) => {
+  const meta = ERROR_METADATA[errorCode];
+
+  res.status(statusCode).json({
+    success: false,
+    error: {
+      id: meta.id,
+      code: errorCode,
+      message: meta.message,
+    },
+  });
+};
---
+++ backend/src/helpers/returnCall/success.returnCall.ts
@@ -0,0 +1,15 @@
+import { Response } from 'express';
+import { HttpStatus } from '../../types/errorCodes';
+
+export const successReturnCall = (
+  res: Response,
+  statusCode: HttpStatus,
+  data: any,
+) => {
+  res.status(statusCode).json({
+    success: true,
+    data,
+  });
+
+  return;
+};
