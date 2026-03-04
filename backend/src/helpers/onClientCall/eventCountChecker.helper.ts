cc8bc7fcf21da9b9ec4733fbade53084c76bba80
scrom <formain337@gmail.com>
2026-03-04 08:31:52 +0000

    documented

--- backend/src/helpers/onClientCall/eventCountChecker.helper.ts
+++ backend/src/helpers/onClientCall/eventCountChecker.helper.ts
@@ -22,15 +22,20 @@ const EVENT_LIMITS_BY_TIER: Record<SubscriptionTier, EventTierLimitConfig> = {
 } as const;
 
 export const eventCountChecker = (user: UserWithDetails): boolean => {
+  // Get users subscription tier
   const tier = user.billing?.subscription_tier;
 
+  // If tier does not exist, throw err
   if (!tier) {
     throw new Error('Subscription tier missing');
   }
 
+  // Get tier limits acc to the tier of user
   const tierLimits = EVENT_LIMITS_BY_TIER[tier];
 
+  // Get users events used
   const userEventCount = user.eventsUsed ?? 0;
 
+  // If users event count >= max events limit, return true, else return false
   return userEventCount >= tierLimits.maxEvents;
 };
