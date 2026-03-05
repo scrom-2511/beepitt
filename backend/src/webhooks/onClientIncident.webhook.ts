88cfcf2cfb0ddde22c23a67c0be0547ff6974d42
scrom <formain337@gmail.com>
2026-03-05 20:06:29 +0000

    update: updated

--- backend/src/webhooks/onClientIncident.webhook.ts
+++ backend/src/webhooks/onClientIncident.webhook.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from 'express';
 import { prisma } from '../database/prismaClient';
 import { billingChecker } from '../helpers/billing/billingChecker.helper';
-import { eventCountChecker } from '../helpers/onClientCall/eventCountChecker.helper';
-import { notificationChannelChatIdsCheckerAndGetter } from '../helpers/onClientCall/notificationChannelChatIdsCheckerAndGetter.helper';
-import { projectExistsChecker } from '../helpers/onClientCall/projectExistsChecker.helper';
+import { enqueueNotificationsOnClientCall } from '../helpers/onClientCall/enqueueNotificationsOnClientCall.helper';
+import { returnCallOnClientCall } from '../helpers/onClientCall/returnCallOnClientCall.helper';
+import { getProjectByProjectName } from '../helpers/project/getProjectByProjectName.helper.';
+import { notificationChannelChatIdsCheckerAndGetter } from '../helpers/project/notificationChannelChatIdsCheckerAndGetter.helper';
 import { errorReturnCall } from '../helpers/returnCall/error.returnCall';
-import { successReturnCall } from '../helpers/returnCall/success.returnCall';
-import { enqueueDiscordNotifications } from '../services/bullmq/producers/discordNotifications.producer';
-import { enqueueTelegramNotifications } from '../services/bullmq/producers/telegramNotifications.producer';
-import { NotificationType, onClientIncidentType } from '../types/dataTypes';
+import { eventCountChecker } from '../helpers/user/eventCountChecker.helper';
+import { getSelectedNotificationChannelsOfUser } from '../helpers/user/getSelectedNotificationChannelsOfUser.helper';
+import { onClientIncidentType } from '../types/dataTypes';
 import { ErrorCode, HttpStatus } from '../types/errorCodes';
 
 export const onClientIncidentWebhook = async (req: Request, res: Response) => {
@@ -24,18 +24,6 @@ export const onClientIncidentWebhook = async (req: Request, res: Response) => {
     // Get user's id
     const userId = req.userId!;
 
-    // Get project name from the req body
-    const projectName = validateData.data.projectName;
-
-    // Check if the project with project name exists or not
-    const projectExists = await projectExistsChecker(projectName, userId);
-
-    // If project does not exist, return
-    if (!projectExists) {
-      errorReturnCall(res, HttpStatus.NOT_FOUND, ErrorCode.PROJECT_NOT_FOUND);
-      return;
-    }
-
     // Check the billing of user, basically it checks whether users paid plan has expired or not
     await billingChecker(userId);
 
@@ -50,6 +38,18 @@ export const onClientIncidentWebhook = async (req: Request, res: Response) => {
       return errorReturnCall(res, HttpStatus.UNAUTHORIZED, ErrorCode.UNAUTHORIZED);
     }
 
+    // Get project name from the req body
+    const projectName = validateData.data.projectName;
+
+    // Check if the project with project name exists or not
+    const projectExists = getProjectByProjectName(projectName, user);
+
+    // If project does not exist, return
+    if (!projectExists) {
+      errorReturnCall(res, HttpStatus.NOT_FOUND, ErrorCode.PROJECT_NOT_FOUND);
+      return;
+    }
+
     // Check if the user exceeds his user count limit
     const eventCountExceeds = eventCountChecker(user);
 
@@ -59,18 +59,13 @@ export const onClientIncidentWebhook = async (req: Request, res: Response) => {
     }
 
     // Check and get the notification channel's chat ids
-    const { discordChatIdsPresent, telegramChatIdsPresent, discordChatIds, telegramChatIds } =
-      await notificationChannelChatIdsCheckerAndGetter(validateData.data.projectName, user);
+    const allChatIdsInfo = notificationChannelChatIdsCheckerAndGetter(validateData.data.projectName, user);
 
-    // If discord chat ids are present, enqueue the discord notification
-    if (discordChatIdsPresent) {
-      await enqueueDiscordNotifications({ userId, data: discordChatIds, type: NotificationType.Incident });
-    }
+    // Get selected notification channels of user
+    const channels = getSelectedNotificationChannelsOfUser(user);
 
-    // If telegram chat ids are present, enqueue the telegram notification
-    if (telegramChatIdsPresent) {
-      await enqueueTelegramNotifications({ userId, data: telegramChatIds, type: NotificationType.Incident });
-    }
+    // Enqueue notifications
+    await enqueueNotificationsOnClientCall(userId, channels, allChatIdsInfo);
 
     // Create an incident in the db
     await prisma.incident.create({
@@ -80,14 +75,8 @@ export const onClientIncidentWebhook = async (req: Request, res: Response) => {
     // Increase events used of user by 1
     await prisma.user.update({ where: { id: userId }, data: { eventsUsed: { increment: 1 } } });
 
-    // If no notification channel is linked, return
-    if (!discordChatIdsPresent && !telegramChatIdsPresent) {
-      errorReturnCall(res, HttpStatus.CONFLICT, ErrorCode.NO_NOTIFICATION_CHANNEL_LINKED);
-    } else {
-      // Else return success
-      successReturnCall(res, HttpStatus.OK, null);
-    }
-
+    // Return to user
+    returnCallOnClientCall(res, allChatIdsInfo);
     return;
   } catch (error) {
     errorReturnCall(res, HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.INTERNAL_SERVER_ERROR);
