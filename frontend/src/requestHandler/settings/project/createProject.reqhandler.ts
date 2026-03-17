133e1f4c9a47222e75d36da40167de377e4b6ca1
scrom <formain337@gmail.com>
2026-03-17 18:45:05 +0000

    update: fix update notificatoin channel reqhandler and create proj req handler

--- frontend/src/requestHandler/settings/project/createProject.reqhandler.ts
+++ frontend/src/requestHandler/settings/project/createProject.reqhandler.ts
@@ -1,24 +1,21 @@
 import { BACKEND_URL } from '@/config/app.config';
+import type { ProjectNameType } from '@/types/dataTypes';
 import axios from 'axios';
+import type z from 'zod';
 
-type UpdateNotificationChannelsPayload = {
-  channels: ('slack' | 'email' | 'telegram' | 'discord')[];
-};
-
-export const updateNotificationChannelsHandler = async (data: UpdateNotificationChannelsPayload): Promise<void> => {
+export const createProjectHandler = async (data: z.infer<typeof ProjectNameType>): Promise<void> => {
   try {
-    const res = await axios.patch(BACKEND_URL + '/user/updateNotificationChannels', data, { withCredentials: true });
+    const res = await axios.post(BACKEND_URL + '/user/createProject', data, { withCredentials: true });
 
     if (res.data.success) {
       return;
     }
 
-    throw new Error(res.data.error?.message || 'Failed to update notification channels');
+    throw new Error(res.data.error?.message || 'Failed to fetch incidents');
   } catch (err: any) {
     if (axios.isAxiosError(err)) {
       throw new Error(err.response?.data?.error?.message || err.message);
     }
-
     throw new Error('There was an unknown error, please try again.');
   }
 };
