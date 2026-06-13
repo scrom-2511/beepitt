2a588071a56fb016e5c17fa089d6f06915d2038d
scrom-2511 <formain337@gmail.com>
2026-06-13 22:34:04 +0530

    fix: types work properly

--- backend/src/helpers/project/getProjectByProjectName.helper..ts
+++ backend/src/helpers/project/getProjectByProjectName.helper..ts
@@ -1,3 +1,4 @@
+import { prisma } from '../../database/prismaClient';
 import {
   Project,
   ProjectWithContactDetails,
@@ -5,10 +6,28 @@ import {
   UserWithBillingConfigurationProjectContactDetails,
 } from '../../types/prismaTypes';
 
+export function hasUserWithProjectContactDetails(
+  user: UserWithBillingConfigurationProject | UserWithBillingConfigurationProjectContactDetails,
+): user is UserWithBillingConfigurationProjectContactDetails {
+  return (
+    user.project.length > 0 &&
+    'contactDetails' in user.project[0]
+  );
+}
+
+export function hasProjectContactDetails(
+  project: Project | ProjectWithContactDetails,
+): project is ProjectWithContactDetails {
+  return 'contactDetails' in project;
+}
+
 export const getProjectByProjectName = (
   projectName: string,
   user: UserWithBillingConfigurationProjectContactDetails | UserWithBillingConfigurationProject,
-): Project | ProjectWithContactDetails | undefined => {
+): Project | ProjectWithContactDetails | undefined | null => {
   // Find the project. If exists return the project, else return undefined
-  return user.project.find((project) => project.projectName.toLowerCase() === projectName.toLowerCase());
+  if (hasUserWithProjectContactDetails(user)) {
+    return user.project.find((project) => project.projectName.toLowerCase() === projectName.toLowerCase()) as ProjectWithContactDetails;
+  }
+  return user.project.find((project) => project.projectName.toLowerCase() === projectName.toLowerCase()) as Project;
 };
